a6f861ab07493d6956eaeec4b2847d8a18c1ab89
Lon Bitner <lonbitner@gmail.com>
2020-04-22 09:44:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-    shipping_manifest["oil paintings"]
+    shipping_manifest["oil"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -26,15 +26,17 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
+                  "muskets" => 2
+                  "gun powder" => 4
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-  shipping_manifest["muskets"] = 2
+
 
   # add 4 gun powder to the shipping_manifest hash below
 
-  shipping_manifest["gun powder"] = 4
+
 
   # return the shipping_manifest hash below
-  return shipping_manifest
+
 end
